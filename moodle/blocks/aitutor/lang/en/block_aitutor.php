a208005f5b424a5eedbbf433a777b6035fe96165
Luca12341234 <lucamagrini1234@gmail.com>
2026-05-02 22:22:53 +0200

    Add Codespaces environment and Moodle AI Tutor block

---
+++ moodle/blocks/aitutor/lang/en/block_aitutor.php
@@ -0,0 +1,5 @@
+﻿<?php
+$string['pluginname'] = 'AI Tutor';
+$string['intro'] = 'AI Tutor prototype for the thesis.';
+$string['aitutor:addinstance'] = 'Add a new AI Tutor block';
+$string['aitutor:myaddinstance'] = 'Add a new AI Tutor block to Dashboard';
---
+++ moodle/blocks/aitutor/version.php
@@ -0,0 +1,8 @@
+﻿<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_aitutor';
+$plugin->version = 2026050200;
+$plugin->requires = 2022112800;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0';
